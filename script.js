4286649a689e0992cc16bf22ee84a58e71fbd018
a9261020 <fish840212@yahoo.com.tw>
2021-12-14 22:49:35 +0800

    add formosa

--- script.js
+++ script.js
@@ -24,17 +24,19 @@ let app = new Vue({
       h4: "Fronted Engineer",
     },
     about: {
-      h1: "About Me. ｜ 你有多自律 就有多自由",
-      h2: "James ｜ 李哲宇",
+      h1: "James ｜ 李哲宇",
+      h2: "",
       p: [
         "　",
-        "畢業於臺北商業大學-資訊管理系",
-        "　",
-        "畢業後先於WiseTech Global擔任兩年客服顧問，後來自學程式轉職為前端工程師。",
+        "畢業於臺北商業大學-資訊管理系 (2013-09 ~ 2017-06)",
         "　",
         "曾在筆記網路負責維護商城頁面、開發新功能(jQuery)",
         "　",
-        "現於哲煜科技，開發專案，比漾廣場官網翻新(Nuxt.js)",
+        "現於哲煜科技，開發專案。",
+        "　",
+        "已上線：比漾廣場官網(Nuxt.js)、寶島鐘錶(Nuxt.js)",
+        "　",
+        "開發中：Dolfan (Nuxt.js，Socket.io)",
       ],
       i: [
         {
@@ -98,15 +100,16 @@ let app = new Vue({
           itemH6: "2021-02-17 ~ 至今",
           itemLi: [
             "* 獨立完成",
-            "比漾廣場官網、商城、文章系統",
-            "哲煜科技官網"
+            "比漾廣場官網、商城、文章系統 (Nuxt.js)",
+            "寶島鐘錶(Nuxt.js)",
+            "哲煜科技官網 (jQuery)",
           ],
         },
         {
           itemH5: "Biji 筆記網路 ｜ 前端工程師",
           itemH6: "2020-07-21 ~ 2021-01-29",
           itemLi: [
-            "* 作品",
+            "jQuery, Gulp, SASS",
             "筆記商城維護",
             "籃球筆記維護",
             "1990官網維護",
@@ -120,9 +123,6 @@ let app = new Vue({
             "＊ 特殊經歷",
             "寫了一隻字串處理程式(CRM Mail convert)，將例行性作業省了 90% 的時間",
             "安排、主持 新功能－電子發票說明會",
-            "因服務品質優良被客戶寄送了一箱芒果",
-            "＊ 接聽電話，解決客戶問題",
-            "第一時間與客戶接觸，安撫客人情緒，了解客戶對於系統上的問題，協助解決相關問題",
             "＊ 擔任PM與客戶洽談新需求/功能",
             "詢問客戶的需求，了解之後先畫出初步的流程圖，確認傳送格式、資料欄位，並請研發部評估工時，請業務部報價",
             "目前完成專案：ASUS Tracking，CCN 提單傳送，客戶美國站點EDI維護",
@@ -130,8 +130,6 @@ let app = new Vue({
             "客戶有人員調動時，會向我司提出系統教育訓練，會外出至客戶公司當講師",
             "＊ 報表修改",
             "依照客戶的需求使用c#、My Sql語法抓出系統中的欄位，轉換成客戶需要呈現的樣子",
-            "＊ 測試程式",
-            "更版前、新需求做完之後都必須對程式做測試，維持程式的品質",
           ],
         },
       ],
@@ -144,8 +142,6 @@ let app = new Vue({
           itemH5: "臺北商業大學 ｜ 資訊管理系",
           itemH6: "2013-09 ~ 2017-06",
           itemLi: [
-            "＊ 專題製作 － Gift Friends",
-            "製作文件、流程圖、網頁、PPT、上台報告",
             "＊ 資管系學會財務長",
             "掌控各大/小活動的預算及支出，與各組溝通如何在不把預算刪減的情況下展現出我們最好的成果。大活動：五系聯合聖誕舞會、資管系財稅系聯合系烤、資管送舊…等",
             "＊ 資管系迎新活動長",
@@ -170,11 +166,10 @@ let app = new Vue({
         {
           github: "",
           img: "./images/works/Nuxt.js-Beyond(official).jpg",
-          herf: "https://beyondbeyond.com.tw/official",
+          href: "https://beyondbeyond.com.tw/official",
           itemTitle: "比漾廣場 - (官網)",
-          itemDetails: "Nuxt.js、SCSS",
+          itemDetails: "Nuxt.js、SCSS、Google Calendar API",
           funcTions: [
-            "比漾都是獨立完成，顧只挑其中功能講解",
             "AWD架構",
             "透過SSR優化SEO",
             "會員抽獎功能(免費及扣點)",
@@ -186,7 +181,7 @@ let app = new Vue({
         {
           github: "",
           img: "./images/works/Nuxt.js-Beyond(content).jpg",
-          herf: "https://beyondbeyond.com.tw/",
+          href: "https://beyondbeyond.com.tw/",
           itemTitle: "比漾廣場 - (文章)",
           itemDetails: "Nuxt.js、SCSS、Canvas",
           funcTions: [
@@ -202,7 +197,7 @@ let app = new Vue({
         {
           github: "",
           img: "./images/works/Nuxt.js-Beyond(shop).jpg",
-          herf: "https://beyondbeyond.com.tw/shop",
+          href: "https://beyondbeyond.com.tw/shop",
           itemTitle: "比漾廣場 - (商城)",
           itemDetails: "Nuxt.js、SCSS",
           funcTions: [
@@ -216,38 +211,22 @@ let app = new Vue({
           extensions: [],
         },
         {
-          github: "https://github.com/a9261020/James_Shop",
-          img: "./images/works/Vue.js-Jamess Shop.jpg",
-          herf: "https://james-shop0310.herokuapp.com/",
-          itemTitle: "James's Shop - (前後端)",
-          itemDetails: "Vuex、Express、Mongoose、heroku",
+          github: "",
+          img: "./images/works/Nuxt.js-Formosa.png",
+          href: "https://www.formosatimes.com.tw/",
+          itemTitle: "寶島鐘錶",
+          itemDetails: "Nuxt.js、SCSS、Google Map API",
           funcTions: [
-            "Vue.js",
-            "sessionStorage儲存登入token，並寫入vuex",
-            "商品類別、名稱搜尋",
-            "購物車、我的最愛",
-            "終極密碼、優惠券",
-            "會員訂單、模擬金流付款",
-            "訪客、會員留言板",
-            "Node.js",
-            "會員註冊、登入、臉書登入",
-            "商品、訂單、會員、優惠券、留言板，新增編輯刪除",
+            "Google Map 功能(門市查詢)",
+            "限定國外IP的部分權限",
+            "部分功能重構及優化",
           ],
-          extensions: [
-            "Vuex",
-            "Vue-Router",
-            "Vue-loading-overlay",
-            "Vue-awesome-swiper",
-            "Vee-validate",
-            "sweetalert",
-            "axios",
-            "Bootstrap",
-          ],
+          extensions: [],
         },
         {
           github: "https://github.com/a9261020/vue_jest_demo",
           img: "./images/works/Vue.js-vue_jest_demo.jpg",
-          herf: "https://a9261020.github.io/vue_jest_demo/#/",
+          href: "https://a9261020.github.io/vue_jest_demo/#/",
           itemTitle: "vue-jest-demo",
           itemDetails: "vue-jest、vue-router、vuex",
           funcTions: [
@@ -267,7 +246,7 @@ let app = new Vue({
         {
           github: "https://github.com/a9261020/restCountriesApiWithColorThemeSwticher",
           img: "./images/works/Vue.js-rest_countries_api.jpg",
-          herf: "https://rest-countries-api-with-color-theme-swticher.vercel.app/",
+          href: "https://rest-countries-api-with-color-theme-swticher.vercel.app/",
           itemTitle: "rest-countries-api",
           itemDetails: "vue3, vue-router, compositionAPI",
           funcTions: [
